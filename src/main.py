858e6024834974481ebf9edd7fc5d9a34ed89a85
Nathan Hoche <hochenat@gmail.com>
2025-06-20 23:56:45 +0200

    Add tqdm for better follow up

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import json
 from colorama import init
 import os
+from tqdm import tqdm
 
 init()
 
@@ -19,31 +20,27 @@ class Processor:
         self.di = DatasetInteraction(dataset_path=self.config['dataset_path'])
         self.llm_handler = LLMHandler(model_name=self.config['model'], verbose=verbose)
 
+
     def process(self):
         print("Processing datasets...")
-        for csv_file, df in self.di.dataframes.items():
-            print(f"Processing {csv_file} ({len(df)} rows X {len(df.columns)} columns)")
-            
-            description = self.llm_handler.ask(
-                query=f"Make a description of this dataset: {csv_file}, which has {len(df)} rows and {len(df.columns)} columns. The different columns names are {', '.join(df.columns.tolist())}.",
-            )
+        for csv_file, df in tqdm(self.di.dataframes.items(), desc="CSV Files"):
+            infos = {
+                "Description": f"Make a description of this dataset: {csv_file}, which has {len(df)} rows and {len(df.columns)} columns. The different columns names are {', '.join(df.columns.tolist())}.",
+                "Possibility": f"Can you list me the different study I can make for this dataset: {csv_file}, which has {len(df)} rows and {len(df.columns)} columns. The different columns names are {', '.join(df.columns.tolist())}.",
+                "Visualization": f"Make a python code to visualize the following file using pandas and matplotlib: {csv_file} ({len(df)} rows and {len(df.columns)} columns). \
+                You can generate multiple graph, etc... but use all the column you can! The different columns names are {', '.join(df.columns.tolist())}."
+            }
 
-            possibility = self.llm_handler.ask(
-                query=f"Can you list me the different study I can make for this dataset: {csv_file}, which has {len(df)} rows and {len(df.columns)} columns. The different columns names are {', '.join(df.columns.tolist())}.",
-            )
-            
-            visualization = self.llm_handler.ask(
-                query=f"Make a python code to visualize the following file using pandas and matplotlib: {csv_file} ({len(df)} rows and {len(df.columns)} columns). \
-                You can generate multiple graph, etc... but use all the column you can! The different columns names are {', '.join(df.columns.tolist())}.",
-            )
-
-            summary = f"# {csv_file}\n# Description:\n{description}\n# Possibilities:\n{possibility}\n# Visualization:\n{visualization}\n"
+            summary = f"# {csv_file}\n"
+            for key, query in tqdm(infos.items(), desc=f"Info Gathering ({csv_file})"):
+                response = self.llm_handler.ask(query=query)
+                summary += f"\n## {key}:\n{response}\n"
 
             with open(f'out/{csv_file}.md', 'w+') as out_file:
                 out_file.write(summary)
 
 
 if __name__ == "__main__":
-    processor = Processor(verbose=True)
+    processor = Processor(verbose=False)
     processor.process()
     
